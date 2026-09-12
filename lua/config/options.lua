2c088a37f0f0c952b904fd09e5c8e78844adb98c
SHAIKH-AZAZ <azazshaikh2703@gmail.com>
2026-09-12 17:32:12 +0530

    keymaps and options update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.swapfile = false
+
+vim.opt.wrap = true
+vim.g.codeium_os = "Darwin"
+vim.g.codeium_arch = "arm64"
+vim.opt.foldmethod = "manual"
